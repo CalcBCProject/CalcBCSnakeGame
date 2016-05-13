6609fbcde05e3c4efcff17b289db1a50dba77fd9
forteddyt <forteddyt@gmail.com>
2016-05-13 13:47:34 -0400

    Added left/right/up/down control capabilities to snake

--- app/src/main/java/com/dangilbert98gmail/pi_thon/ControlSection.java
+++ app/src/main/java/com/dangilbert98gmail/pi_thon/ControlSection.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -18,93 +19,38 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ControlSection extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private PlayScreen playActivity = PlayScreen.getInstance();
-
-    private OnFragmentInteractionListener mListener;
-
-    public ControlSection() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ControlSection.
-     */
-    // TODO: Rename and change types and number of parameters
-
-    public static ControlSection newInstance(String param1, String param2) {
-        ControlSection fragment = new ControlSection();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Button leftButton, rightButton, upButton, downButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control_section, container, false);
+        View v = inflater.inflate(R.layout.fragment_control_section, container, false);
+
+        leftButton = (Button)(v.findViewById(R.id.LeftButton));
+        rightButton = (Button)(v.findViewById(R.id.RightButton));
+        upButton = (Button)(v.findViewById(R.id.UpButton));
+        downButton = (Button)(v.findViewById(R.id.DownButton));
+
+        leftButton.setOnClickListener( new buttonListener(SnakeDirection.Left));
+        rightButton.setOnClickListener( new buttonListener(SnakeDirection.Right));
+        upButton.setOnClickListener( new buttonListener(SnakeDirection.Up));
+        downButton.setOnClickListener( new buttonListener(SnakeDirection.Down));
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public class buttonListener implements View.OnClickListener{
+        private SnakeDirection direction;
+
+        public buttonListener(SnakeDirection d){
+            direction = d;
         }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        @Override
+        public void onClick(View v) {
+            playActivity.setSnakeDirection(direction);
         }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
