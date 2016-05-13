f857e0ca82ca82dec7e1c3caa89f2ed3a1940695
Dan Gilbert <Dan Gilbert>
2016-05-13 09:45:40 -0400

    Created play screen

---
+++ app/src/main/java/com/dangilbert98gmail/pi_thon/PlayScreen.java
@@ -0,0 +1,18 @@
+package com.dangilbert98gmail.pi_thon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PlayScreen extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_play_screen );
+	}
+
+}
