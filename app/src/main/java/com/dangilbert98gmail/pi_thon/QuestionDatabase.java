fd7636e6ff0ac49abedc8eb5636519d6705db0fc
Dan Gilbert <Dan Gilbert>
2016-05-15 22:42:21 -0400

    question database *incomplete*

--- app/src/main/java/com/dangilbert98gmail/pi_thon/QuestionDatabase.java
+++ app/src/main/java/com/dangilbert98gmail/pi_thon/QuestionDatabase.java
@@ -1,18 +1,27 @@
 package com.dangilbert98gmail.pi_thon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Teddy on 5/15/2016.
  */
-public class QuestionDatabase {
-    private final int NUM_QUESTIONS = 40;
+public final class QuestionDatabase {
+    private static final int NUM_QUESTIONS = 40;
+    private static ArrayList<Question> ORIGIONAL_QUESTIONS = new ArrayList<Question>();
+	private static ArrayList<Question> questions = new ArrayList<Question>();
 
-    public static Question getRandomQuestion(){
+    public static Question getRandomQuestion()
+    {
+	    if( ORIGIONAL_QUESTIONS.size() == 0 )
+	    {
+		    inhabitOriginal();
+	    }
         return getQuestion((int)(Math.random() * NUM_QUESTIONS));
     }
 
-    public static void resetDatabase(){
+    public static void resetDatabase()
+    {
 
     }
 
@@ -23,4 +32,19 @@ public class QuestionDatabase {
 
         return q;
     }
+	public static void restartDatabase()
+	{
+		inhabitOriginal();
+		for( int i = 0; i < NUM_QUESTIONS; i++ )
+		{
+			questions.add(ORIGIONAL_QUESTIONS.get(i) );
+		}
+	}
+	private static void inhabitOriginal()
+	{
+		if( ORIGIONAL_QUESTIONS.size() == 0 )
+		{
+			//fills Orional
+		}
+	}
 }
