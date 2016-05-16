e12914c99eb3712d21d37fa97454115b27bf5e33
forteddyt <forteddyt@gmail.com>
2016-05-15 22:04:34 -0400

    Made NUM_QUESTIONS static

--- app/src/main/java/com/dangilbert98gmail/pi_thon/QuestionDatabase.java
+++ app/src/main/java/com/dangilbert98gmail/pi_thon/QuestionDatabase.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Created by Teddy on 5/15/2016.
  */
 public class QuestionDatabase {
-    private final int NUM_QUESTIONS = 40;
+    private static final int NUM_QUESTIONS = 40;
 
     public static Question getRandomQuestion(){
         return getQuestion((int)(Math.random() * NUM_QUESTIONS));
