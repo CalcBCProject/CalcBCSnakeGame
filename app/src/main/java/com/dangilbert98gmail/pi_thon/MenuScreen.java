64b1b1560f1ae30a95a9e06d6c80bac1e51d3a23
Dan Gilbert <Dan Gilbert>
2016-05-17 11:22:45 -0400

    intents, better gifs = mas babes ezpz

--- app/src/main/java/com/dangilbert98gmail/pi_thon/MenuScreen.java
+++ app/src/main/java/com/dangilbert98gmail/pi_thon/MenuScreen.java
@@ -1,18 +1,57 @@
 package com.dangilbert98gmail.pi_thon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MenuScreen extends AppCompatActivity
 {
+
+	private Button startGame, about;
+	private ImageButton help;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_menu );
+
+		startGame = (Button) findViewById( R.id.startButton );
+		startGame.setOnClickListener( new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent i = new Intent( MenuScreen.this, PlayScreen.class );
+
+				startActivity( i );
+			}
+		} );
+
+		about = (Button) findViewById( R.id.aboutButton );
+		about.setOnClickListener( new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				//about stuff
+			}
+		} );
+
+		help = (ImageButton) findViewById( R.id.helpButton );
+		help.setOnClickListener( new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				//help stuff
+			}
+		} );
 	}
 
 }
